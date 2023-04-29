0d9f960f5b2c9285533f155e3e446d08ddbdeb5b
error404-2 <kapwas@gmail.com>
2023-04-29 23:19:45 +0300

    feat: add textarea

---
+++ index.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body");
+const textarea = document.createElement("textarea");
+const key = document.createElement("div");
+textarea.className = "textarea";
+textarea.rows = "7";
+textarea.cols = "50"
+body.append(textarea);
+
